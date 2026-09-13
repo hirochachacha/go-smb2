2574cb49be2e750508355d374b70d959187ce263
Hiroshi Ioka <hirochachacha@gmail.com>
2026-09-13 20:22:28 +0530

    fix: support TargetSPN in credentials and fix cancellation test
      - add TargetSPN field to NTLMCredential and KerberosCredential
      - pass configured TargetSPN when constructing credentials in tests
      - use dedicated share and pass canceled context in
        TestContextCancellation

--- credentials.go
+++ credentials.go
@@ -21,25 +21,35 @@ type NTLMCredential struct {
 	Hash        []byte
 	Domain      string
 	Workstation string
+	TargetSPN   string
 }
 
 func (c NTLMCredential) NewInitiator(_ context.Context, serverName string) (Initiator, error) {
+	spn := c.TargetSPN
+	if spn == "" {
+		spn = "cifs/" + serverName
+	}
 	return &NTLMInitiator{
 		User:        c.User,
 		Password:    c.Password,
 		Hash:        append([]byte(nil), c.Hash...),
 		Domain:      c.Domain,
 		Workstation: c.Workstation,
-		TargetSPN:   "cifs/" + serverName,
+		TargetSPN:   spn,
 	}, nil
 }
 
 // KerberosCredential creates Kerberos initiators using the supplied Kerberos
 // client. The caller remains responsible for destroying the Kerberos client.
 type KerberosCredential struct {
-	Client *krbclient.Client
+	Client    *krbclient.Client
+	TargetSPN string
 }
 
 func (c KerberosCredential) NewInitiator(_ context.Context, serverName string) (Initiator, error) {
-	return &KerberosInitiator{Client: c.Client, TargetSPN: "cifs/" + serverName}, nil
+	spn := c.TargetSPN
+	if spn == "" {
+		spn = "cifs/" + serverName
+	}
+	return &KerberosInitiator{Client: c.Client, TargetSPN: spn}, nil
 }
